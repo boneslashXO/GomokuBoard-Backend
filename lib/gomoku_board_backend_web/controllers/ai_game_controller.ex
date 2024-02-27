92793297793a01dcfcc5a9ba6a33909503d48c12
Lukáš Souček <lukas.sportovec@seznam.cz>
2024-02-27 22:25:06 +0100

    thanks to ChatGPT important clause aadded

--- lib/gomoku_board_backend_web/controllers/ai_game_controller.ex
+++ lib/gomoku_board_backend_web/controllers/ai_game_controller.ex
@@ -3,7 +3,6 @@ defmodule GomokuBoardBackendWeb.AIGameController do
 
   def create(conn, %{"start" => move_params}) do
     # Implement your logic here
-
     json(conn, %{response: "Your logic result here"})
   end
 end
