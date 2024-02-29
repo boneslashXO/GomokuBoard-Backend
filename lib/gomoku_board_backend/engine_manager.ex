3bc084e3544ee08c52adb2a4481bffec5fcafda1
boneslashXO <lukas.sportovec@seznam.cz>
2024-02-29 09:54:23 +0100

    formatting

--- lib/gomoku_board_backend/engine_manager.ex
+++ lib/gomoku_board_backend/engine_manager.ex
@@ -17,6 +17,7 @@ defmodule GomokuBoardBackend.EngineManager do
   # Handling the call to start the engine and setup the board
   def handle_call({:start_engine_and_setup_board, command}, _from, state) do
     engine_path = System.get_env("ENGINE_PATH") || "path/to/pbrain.exe"
+
     port =
       Port.open({:spawn_executable, engine_path}, [
         :binary,
@@ -24,22 +25,25 @@ defmodule GomokuBoardBackend.EngineManager do
         :use_stdio,
         :stderr_to_stdout
       ])
+
     Port.command(port, command)
     {:reply, :ok, Map.put(state, :port, port)}
   end
 
   def handle_info({port, {:data, data}}, state) do
-  # Your existing logic to broadcast engine output
-  broadcast_engine_output(data)
-  {:noreply, state}
+    # Your existing logic to broadcast engine output
+    broadcast_engine_output(data)
+    {:noreply, state}
   end
 
   defp broadcast_engine_output(data) do
     # Parse or transform data as needed
-    IO.puts(data);
+    IO.puts(data)
     parsed_data = String.trim(data)
 
     # Broadcast the data to the channel
-    GomokuBoardBackendWeb.Endpoint.broadcast("AIgame:lobby", "engine_output", %{output: parsed_data})
+    GomokuBoardBackendWeb.Endpoint.broadcast("AIgame:lobby", "engine_output", %{
+      output: parsed_data
+    })
   end
 end
--- lib/gomoku_board_backend_web/channels/ai_game_channel.ex
+++ lib/gomoku_board_backend_web/channels/ai_game_channel.ex
@@ -6,14 +6,13 @@ defmodule GomokuBoardBackendWeb.AIGameChannel do
   end
 
   def handle_in("start_game", _message, socket) do
-  GomokuBoardBackend.EngineManager.start_engine_and_setup_board("START 15\n")
-  {:noreply, socket}
+    GomokuBoardBackend.EngineManager.start_engine_and_setup_board("START 15\n")
+    {:noreply, socket}
   end
 
   def handle_in("send_something", %{"content" => content}, socket) do
-  #EngineManager.start_engine_and_setup_board("START 15\n")
-       IO.puts("Message received: #{content}")
-  {:noreply, socket}
+    # EngineManager.start_engine_and_setup_board("START 15\n")
+    IO.puts("Message received: #{content}")
+    {:noreply, socket}
   end
-
 end
